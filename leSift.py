7c5d1c1cffb1d90dcccd622368b5a04d46997eeb
MINSOO KANG <stengine2@gmail.com>
2021-05-23 15:13:49 +0900

    Add files via upload

--- leSift.py
+++ leSift.py
@@ -3,18 +3,38 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img = cv.imread('home - Copy.jpg')
-
-gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-sift = cv.SIFT_create()
-
-kp = sift.detect(gray,None)
-img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-print(np.shape(img))
-cv.imwrite('sift_keypoints.jpg',img)
-kp, des = sift.detectAndCompute(gray,None)
 
 
-np.shape(des)
+class siftBuilder():
+    def __init__(self):
+        images=[]
+        descriptors = []
 
-codebook, distortion = vq.kmeans(des, 3)
+    def add_image(self, im_arr):
+        for i in im_arr:
+            images.append(i)
+
+    def sift(self):
+        descriptors=[]
+        for i in self.images:
+            kp,des = sift.detetAndCompute(i)
+            descriptors.append(des)
+        return np.array(descrptors)
+
+
+if __name__ == "__main__":
+    img = cv.imread('urban.jpg')
+
+    gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    sift = cv.SIFT_create()
+
+    kp = sift.detect(gray,None)
+    img=cv.drawKeypoints(img,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    print(np.shape(img))
+    cv.imwrite('sift_keys.jpg',img)
+    kp, des = sift.detectAndCompute(gray,None)
+
+
+    np.shape(des)
+
+    codebook, distortion = vq.kmeans(des, 3)
